d792fa9ad09d3eccfe06b1489594e4bcc7e36602
Michal Horn <michal@apartman.cz>
2020-08-07 19:20:42 +0200

    Add base header file needed when including the generated fonts

---
+++ lw_font.h
@@ -0,0 +1,37 @@
+/*
+ * ili9341_font.h
+ *
+ *  Created on: 20. 7. 2020
+ *      Author: E9990291
+ */
+
+#ifndef LW_FONT_H_
+#define LW_FONT_H_
+
+#include "stdint.h"
+#include "stdbool.h"
+#include "wchar.h"
+
+typedef struct {
+	const uint8_t width;
+	const uint8_t offset_x;
+	const uint8_t offset_y;
+	const uint8_t* pixmap;
+} lw_char_def_t;
+
+typedef struct {
+	const wchar_t char_code;
+	const lw_char_def_t char_def;
+} lw_char_map_t;
+
+typedef struct {
+	const char* family;
+	const uint8_t size;
+	const uint8_t height;
+	const char* style;
+	const uint16_t chars_cnt;
+	const lw_char_map_t *chars;
+	const bool inv;
+} lw_font_t;
+
+#endif /* LW_FONT_H_ */
